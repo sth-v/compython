cab3092e48d528622ab62882de33de2d95ad7c29
sth-v <aa@contextmachine.ru>
2022-01-13 08:03:08 +0300

    restruct app.py

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
-from setuptools import setup
+import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setup(
-    name='compython',
-    version='0.0.4',
-    packages=['src'],
-    url='https://github.com/sth-v/compython.git',
-    license='MIT',
-    author='Andrew Astakhov',
-    author_email='aw.astakhov@gmail.com',
-    description='some geometry pack',
+setuptools.setup(
+    name="compython",
+    version='0.0.5',
+    author="Andrew Astakhov",
+    author_email="aw.astakhov@gmail.com",
+    description="compython",
     long_description=long_description,
-    long_description_content_type="text/markdown"
+    long_description_content_type="text/markdown",
+    url="https://github.com/sth-v/compython",
+    project_urls={
+        "Bug Tracker": "https://github.com/sth-v/compython/issues",
+    },
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
