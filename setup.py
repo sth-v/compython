440cf2e7004d6f50e6c4f1322d2694f688779983
sth-v <aa@contextmachine.ru>
2022-01-13 07:20:34 +0300

    yy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='compython',
-    version='0.0.1',
+    version='0.0.3',
     packages=['src'],
     url='https://github.com/sth-v/compython.git',
     license='MIT',
